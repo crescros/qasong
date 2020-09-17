4a14c68d87b4f6532bad50e283ee130fa87579b4
ian <ian@unichord-apps.com>
2020-09-17 13:35:46 -0400

    axios and search endpoing

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const app = express();
 app.use(cors())
 app.use(express.static('public'))
 
+
+
+app.get('/api/search', (req, res) => {
+    const searchTerm = req.query.search        
+    const apiKey = "AIzaSyDhCHzOQKYw8tajxF33m04RmxLNrufEUnI"
+
+    if (!apiKey){
+        res.send('Error: config file missing youtube API key')
+        return
+    }
+
+    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchTerm}&key=${apiKey}`).then(response => {
+        const youtubeID = response.data.items[0].id.videoId
+        const youtubeURL = 'https://www.youtube.com/watch?v=' + youtubeID
+        res.send(youtubeURL)
+    })
+})
+
 // start server
 const port = process.env.PORT || 3016
 app.listen(port, () => {
