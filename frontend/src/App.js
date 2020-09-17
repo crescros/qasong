c205780ab8b0accd12efe1f781a8a0b550616f67
ian <ian@unichord-apps.com>
2020-09-17 14:47:51 -0400

    search input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from "react";
-import { Container, Button, Typography } from "@material-ui/core"
+import React, { useState } from "react";
+import { Container, Button, Typography, TextField } from "@material-ui/core"
 import { getYoutubeIdFromSearch } from './functions'
 
 
 const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState('')
+
+
+    const handleSearchTermInput = (e) => {
+        setSearchTerm(e.target.value)
+    }
     
-    const handleClickPlay = () => {
-        console.log(getYoutubeIdFromSearch('hello'))
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const id = await getYoutubeIdFromSearch(searchTerm)
+        console.log(id)
     }
 
     return (<Container>
-        <Typography>Music App</Typography>
-        <Button onClick={handleClickPlay}>Play</Button>
+        <Typography variant='h1'>Music App</Typography>
+        <form onSubmit={handleSubmit}>
+            <TextField id="outlined-basic" label="Search" variant="outlined" onChange={handleSearchTermInput} />
+            <Button>Play</Button>
+        </form>
     </Container>
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import regeneratorRuntime from 'regenerator-runtime'
 
 ReactDOM.render(<App />, document.getElementById('app'));
