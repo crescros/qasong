e8cd6f3f271636017864beb0f178b752737fb190
ian <ian@unichord-apps.com>
2020-09-17 14:17:53 -0400

    functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Container, Button, Typography } from "@material-ui/core"
 
-export class App extends Component {
-    render() {
-        return (<Container>
-            <Typography>Music App</Typography>
-        </Container>
-        );
-    }
+const App = () => {
+    return (<Container>
+        <Typography>Music App</Typography>
+    </Container>
+    );
+}
 }
 
 export default App;
