8017cf3ec4dd5f1448f18e16b05f9020e55d5e1b
ian <ian@unichord-apps.com>
2020-09-17 15:05:04 -0400

    get video id from api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,47 @@
 import React, { useState } from "react";
-import { Container, Button, Typography, TextField } from "@material-ui/core"
+import { Container, IconButton, Typography, TextField } from "@material-ui/core"
+import { PlayArrow } from "@material-ui/icons"
 import { getYoutubeIdFromSearch } from './functions'
+import YouTube from 'react-youtube'
 
 
 const App = () => {
 
     const [searchTerm, setSearchTerm] = useState('')
+    const [videoId, setVideoId] = useState(undefined)
 
 
     const handleSearchTermInput = (e) => {
         setSearchTerm(e.target.value)
     }
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const id = await getYoutubeIdFromSearch(searchTerm)
-        console.log(id)
+        const response = await getYoutubeIdFromSearch(searchTerm)
+        setVideoId(response.data)
+    }
+
+    const youtubePlayerOptions = {
+        height: '390',
+        width: '640',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        }
     }
 
     return (<Container>
         <Typography variant='h1'>Music App</Typography>
         <form onSubmit={handleSubmit}>
             <TextField id="outlined-basic" label="Search" variant="outlined" onChange={handleSearchTermInput} />
-            <Button>Play</Button>
+            <IconButton><PlayArrow /></IconButton>
         </form>
+        {
+            videoId && <YouTube
+                videoId={videoId}
+                opts={youtubePlayerOptions}
+            />
+        }
     </Container>
     );
 }
