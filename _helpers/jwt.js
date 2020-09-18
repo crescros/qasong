4acd235c7f8ced32d98246cc599eb5a67b07c8f2
callbacc <65503452+callbacc@users.noreply.github.com>
2020-09-18 16:05:26 +0200

    Update jwt.js

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -4,17 +4,14 @@ const expressJwt = require('express-jwt');
 module.exports = jwt;
 
 function jwt() {
-
-
-
     return expressJwt({ secret: process.env.SECRET,  algorithms: ['RS256']  }).unless({
         path:[
             /favicon.ico/, // for dev
-            new RegExp(process.env.APIROOT + '\/'),
-            new RegExp(process.env.APIROOT + '\/app'),
-            new RegExp(process.env.APIROOT + '\/api\/users\/authenticate'),
-            new RegExp(process.env.APIROOT + '\/api\/users\/create'),
-            new RegExp(process.env.APIROOT + '\/api\/animations\/trials')
+            new RegExp('\/'),
+            new RegExp('\/api\/search'),
+            new RegExp('\/api\/users\/authenticate'),
+            new RegExp('\/api\/users\/create'),
+            new RegExp('\/api\/animations\/trials')
         ]
     });
 }
