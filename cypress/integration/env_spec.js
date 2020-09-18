86d6da01bf5b00927de9df2bb844579079737452
ian <ian@unichord-apps.com>
2020-09-18 15:17:23 -0400

    test env variable for types

---
+++ cypress/integration/env_spec.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+describe('.env file', () => {
+    it('is exists', () => {
+      expect(typeof Cypress.env()).to.not.equal(undefined)
+    })
+
+    it('has secret with type: int', () =>{
+      expect(typeof Cypress.env("SECRET")).to.equal("number")
+    })
+    
+    it('has youtube api key', ()=> {
+      expect(typeof Cypress.env("YOUTUBE_API_KEY")).to.equal("string")        
+    })
+  })
