7af87c505c10e218dd6c489dff680edf099382eb
callbacc <65503452+callbacc@users.noreply.github.com>
2020-09-18 17:42:28 +0200

    Update AppBar.js

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import VideoSearch from './VideoSearch'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function ButtonAppBar() {
+export default function ButtonAppBar({ handleSubmitVideoSearch, handleSearchTermInput }) {
     const classes = useStyles();
 
     return (
@@ -31,7 +32,9 @@ export default function ButtonAppBar() {
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         Music App
-          </Typography>
+                    </Typography>
+                    <VideoSearch onSubmit={handleSubmitVideoSearch} handleSearchTermInput={handleSearchTermInput} />
+
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
