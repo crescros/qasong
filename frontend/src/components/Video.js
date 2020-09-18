bf94ec96441dd472f68e68806c2a42183249137e
callbacc <65503452+callbacc@users.noreply.github.com>
2020-09-18 17:30:52 +0200

    Update Video.js

--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -4,8 +4,8 @@ import YouTube from 'react-youtube'
 function Video({ id }) {
 
     const youtubePlayerOptions = {
-        height: '390',
-        width: '640',
+        height: '0',
+        width: '0',
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
             autoplay: 1,
