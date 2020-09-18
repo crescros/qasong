baeb90b564934ec7cd2d40d0e138443cf7794f7f
callbacc <65503452+callbacc@users.noreply.github.com>
2020-09-18 18:38:35 +0200

    Update AppBar.js

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -20,22 +22,38 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function ButtonAppBar({ handleSubmitVideoSearch, handleSearchTermInput }) {
+export default function ButtonAppBar({ handleSubmitVideoSearch, handleSearchTermInput, nowPlaying }) {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Music App
-                    </Typography>
-                    <VideoSearch onSubmit={handleSubmitVideoSearch} handleSearchTermInput={handleSearchTermInput} />
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-between"
+                        alignItems="flex-start"
+                    >
 
-                    <Button color="inherit">Login</Button>
+                        <Grid item xs={1}>
+                            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                                <MenuIcon />
+                            </IconButton>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            { nowPlaying && <Typography>Now Playing: { nowPlaying.title }</Typography> }
+                        </Grid>
+
+                        <Grid item xs={3}>
+                            <VideoSearch onSubmit={handleSubmitVideoSearch} handleSearchTermInput={handleSearchTermInput} />
+                        </Grid>
+                        
+                        <Grid item xs={2}>
+                            <Button color="inherit">Login</Button>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </AppBar>
         </div>
