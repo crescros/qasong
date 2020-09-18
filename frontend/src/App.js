afbf309bee35ad03df7956d742e1576c1afd82e5
callbacc <65503452+callbacc@users.noreply.github.com>
2020-09-18 17:30:45 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
-import { Typography, Grid, CssBaseline } from "@material-ui/core"
+import { Typography, Grid, CssBaseline, Container } from "@material-ui/core"
 import { getYoutubeIdFromSearch } from './functions'
 import Video from './components/Video'
 import VideoSearch from './components/VideoSearch'
 import AppBar from './components/AppBar';
+import VideoCard from './components/videoCard'
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
 
 const App = () => {
     const [ searchTerm, setSearchTerm ] = useState('');
-    const [ videos, setVideos ] = useState([]);
-
+    const [videos, setVideos] = useState([]);
+    const [nowPlaying, setNowPlaying] = useState()    
+    
     const handleSearchTermInput = (e) => {
         setSearchTerm(e.target.value);
     }
@@ -31,21 +33,47 @@ const App = () => {
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline />
-            <AppBar />
-                <Grid container>
-                <Grid item xs={12}>
 
+            {JSON.stringify(nowPlaying)}
+            
+            <AppBar />
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+            >
+                <Grid item xs={12}>
                     <VideoSearch onSubmit={handleSubmitVideoSearch} handleSearchTermInput={handleSearchTermInput} />
                 </Grid>
+                <Container>
                 {
-                    videos.map(video => {
+                    nowPlaying ?
+                        <VideoCard
+                            id={nowPlaying.id}
+                            title={nowPlaying.title}
+                            thumbnailUrl={nowPlaying.thumbnailUrl}
+                        /> : videos.map(video => {
+                        const url = video.snippet.thumbnails.high.url || video.snippet.thumbnails.default.url
+                        const title = video.snippet.title
+
                         return <Grid item xs={12}>
-                            <Typography variant="h6">{video.title}</Typography>
-                            <img src={video.snippet.thumbnails.default.url} />
+                            <VideoCard
+                                id={video.id.videoId}
+                                thumbnailUrl={url}
+                                title={title}
+                                description={video.snippet.description}
+                                setNowPlaying={setNowPlaying}
+                            />
                         </Grid>
                     })
                 }
+                </Container>
             </Grid>
+            
+            {
+                nowPlaying && <Video id={nowPlaying.id} />
+            }
         </ThemeProvider>
     ); 
 }
