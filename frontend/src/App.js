3d4f1f0ff2cbd62ed98f163270a6e3cd3a93a434
callbacc <65503452+callbacc@users.noreply.github.com>
2020-09-18 16:06:00 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,46 @@
 import React, { useState } from "react";
-import { Container, Typography } from "@material-ui/core"
+import { Typography, Grid, CssBaseline } from "@material-ui/core"
 import { getYoutubeIdFromSearch } from './functions'
 import Video from './components/Video'
 import VideoSearch from './components/VideoSearch'
+import AppBar from './components/AppBar';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
 
 const App = () => {
-    const [searchTerm, setSearchTerm] = useState('')
-    const [videoId, setVideoId] = useState(undefined)
+    const [ searchTerm, setSearchTerm ] = useState('');
+    const [ videoId, setVideoId ] = useState(undefined);
 
     const handleSearchTermInput = (e) => {
-        setSearchTerm(e.target.value)
+        setSearchTerm(e.target.value);
     }
 
     const handleSubmitVideoSearch = async (e) => {
-        e.preventDefault()
-        const response = await getYoutubeIdFromSearch(searchTerm)
-        setVideoId(response.data)
+        e.preventDefault();
+        const response = await getYoutubeIdFromSearch(searchTerm);
+        setVideoId(response.data);
     }
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: 'dark'
+        }
+    });
 
-    return (<Container>
-        <Typography variant='h1'>Music App</Typography>
-        <VideoSearch
-            onSubmit={handleSubmitVideoSearch}
-            handleSearchTermInput={handleSearchTermInput}
-        />
-        <Video 
-            id={videoId}
-        />
-    </Container>
-    );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <AppBar />
+                <Grid container>
+                    <Grid item xs = { 12 }>
+                        <VideoSearch onSubmit = { handleSubmitVideoSearch } handleSearchTermInput = { handleSearchTermInput } />
+                    </Grid>    
+                    <Grid item xs = { 12 }>
+                        <Video id = { videoId } />                
+                    </Grid>
+                </Grid>
+        </ThemeProvider>
+    ); 
 }
 
 export default App;
