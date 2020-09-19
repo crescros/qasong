d4b8b1518afbecd2060fa448fd9fbb694e132a64
ian <ian@unichord-apps.com>
2020-09-19 14:31:58 -0400

    auth tests

--- cypress/integration/api_spec.js
+++ cypress/integration/api_spec.js
@@ -9,12 +9,28 @@ describe('seach endpoint', () => {
 
 describe('auth endpoint', () => {
     it('authenticates test user with proper credentials', () => {
-        cy.request('/api/user/authenticate')
+        const postBody = {
+            "username": "testuser",
+            "password": "testpassword123"
+        }
+
+        cy.request('POST', '/api/users/authenticate', postBody)
             .then((response) => {
-                expect(response.body).to.exist
-                expect(response).to.exist
+                expect(response.body.token).to.exist
+            })
+    })
+
+    it('rejects test user with wrong password', () => {
+        const postBody = {
+            "username": "testuser",
+            "password": "testpasswxrd123"
+        }
+
+        cy.request({ method: 'POST', url: '/api/users/authenticate', body: postBody, failOnStatusCode: false })
+            .then((response) => {
+
+                expect(response).to.have.property('status', 400)
             })
     })
 })
 
-//
