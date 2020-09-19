d6f9e4d4317116fea61bfd953fa840d39539d5c3
Jose <josejardimdev@gmail.com>
2020-09-19 17:11:27 +0200

    added the login Modal form

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import NowPlayingInfo from './NowPlayingInfo'
-import VideoSearch from './VideoSearch'
+import NowPlayingInfo from './NowPlayingInfo';
+import VideoSearch from './VideoSearch';
+import LoginModal from './LoginModal';
 
 export default function ButtonAppBar({ handleSubmitVideoSearch, handleSearchTermInput, nowPlaying, setNowPlaying }) {
-
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography style={{ marginRight: '20px' }}>Music App</Typography>
-                <VideoSearch handleSearchTermInput={handleSearchTermInput} handleSubmitVideoSearch={handleSubmitVideoSearch} />
-                <NowPlayingInfo title={nowPlaying && nowPlaying.title} setNowPlaying={setNowPlaying} />
-            </Toolbar>
-        </AppBar>
-
-    );
+	return (
+		<AppBar position="static">
+			<Toolbar display="flex" style={{ justifyContent: 'space-between' }}>
+				<Typography style={{ marginRight: '20px' }}>Music App</Typography>
+				<VideoSearch
+					handleSearchTermInput={handleSearchTermInput}
+					handleSubmitVideoSearch={handleSubmitVideoSearch}
+				/>
+				<NowPlayingInfo title={nowPlaying && nowPlaying.title} setNowPlaying={setNowPlaying} />
+				<LoginModal />
+			</Toolbar>
+		</AppBar>
+	);
 }
