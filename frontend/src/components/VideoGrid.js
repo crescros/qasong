70707c23e5a93ea92067dd0536017e7e6867aa6e
Jose <josejardimdev@gmail.com>
2020-09-20 21:20:55 +0200

    centered cards

--- frontend/src/components/VideoGrid.js
+++ frontend/src/components/VideoGrid.js
@@ -1,53 +1,50 @@
-import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
-import VideoCard from './VideoCard'
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import VideoCard from './VideoCard';
 
-
-console.log(process.env)
+console.log(process.env);
 
 function VideoGrid({ videos, nowPlaying, setNowPlaying, queue, setQueue }) {
+	return (
+		<Grid style={{ marginTop: '60px' }} container direction="row" justify="center" alignItems="center">
+			{videos.map((video) => {
+				const url = video.snippet.thumbnails.high.url || video.snippet.thumbnails.default.url;
+				const title = video.snippet.title;
 
-    return <Grid
-        style={{ marginTop: '20px' }}
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-    >
-        {videos.map(video => {
-            const url = video.snippet.thumbnails.high.url || video.snippet.thumbnails.default.url
-            const title = video.snippet.title
+				const gridItem = (
+					<Grid
+						item
+						xs={12}
+						sm={6}
+						md={6}
+						lg={4}
+						xl={3}
+						key={video.id.videoId}
+						children={
+							<VideoCard
+								id={video.id.videoId}
+								thumbnailUrl={url}
+								title={title}
+								description={video.snippet.description}
+								nowPlaying={nowPlaying}
+								setNowPlaying={setNowPlaying}
+								queue={queue}
+								setQueue={setQueue}
+							/>
+						}
+					/>
+				);
 
-            const gridItem = <Grid item
-                xs={12}
-                sm={6}
-                md={6}
-                lg={4}
-                xl={3}
-                key={video.id.videoId}
-                children={<VideoCard
-                    id={video.id.videoId}
-                    thumbnailUrl={url}
-                    title={title}
-                    description={video.snippet.description}
-                    nowPlaying={nowPlaying}
-                    setNowPlaying={setNowPlaying}
-                    queue={queue}
-                    setQueue={setQueue}
-                    
-                />}
-            />
+				return gridItem;
+			})}
 
-            return gridItem
-        })}
-
-        {
-            videos.length == 0 &&
-            <Typography variant="h1" style={{color:'black'}}>
-                {process.env.REACT_APP_NAME.toUpperCase()}
-        </Typography>
-        }
-    </Grid >
+			{videos.length == 0 && (
+				<Typography variant="h1" style={{ color: 'black' }}>
+					{process.env.REACT_APP_NAME.toUpperCase()}
+				</Typography>
+			)}
+		</Grid>
+	);
 }
 
-export default VideoGrid
+export default VideoGrid;
