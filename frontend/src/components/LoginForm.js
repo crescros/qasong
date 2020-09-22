53782bf8ec69bab4fa751f8fa2e03587db4227a3
Jose <josejardimdev@gmail.com>
2020-09-22 20:10:42 +0200

    changed colours

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Typography, Container, CircularProgress } from '@material-ui/core';
-import { Alert } from '@material-ui/lab/'
+import {
+	Avatar,
+	Button,
+	CssBaseline,
+	TextField,
+	FormControlLabel,
+	Checkbox,
+	Link,
+	Grid,
+	Typography,
+	Container,
+	CircularProgress
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab/';
 import { makeStyles } from '@material-ui/core/styles';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
@@ -25,33 +37,22 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignIn({ handleLoginFormSubmit, error, handleCreateUserClick, message, loading }) {
-
 	const classes = useStyles();
 
 	return (
 		<Container component="main" maxWidth="xs">
 			<CssBaseline />
 			<div className={classes.paper}>
-				{
-					loading && <CircularProgress />
-				}
-				{
-					message && <Alert severity="success">{message}</Alert>
-				}
-				{
-					error && <Alert severity="error">{error}</Alert>
-				}
+				{loading && <CircularProgress />}
+				{message && <Alert severity="success">{message}</Alert>}
+				{error && <Alert severity="error">{error}</Alert>}
 				<Avatar className={classes.avatar}>
 					<LockOutlinedIcon />
 				</Avatar>
 				<Typography component="h1" variant="h5">
 					Sign in
 				</Typography>
-				<form
-					className={classes.form} 
-					noValidate 
-					onSubmit={handleLoginFormSubmit}
-				>
+				<form className={classes.form} noValidate onSubmit={handleLoginFormSubmit}>
 					<TextField
 						variant="outlined"
 						margin="normal"
@@ -74,26 +75,24 @@ export default function SignIn({ handleLoginFormSubmit, error, handleCreateUserC
 						id="password"
 						autoComplete="current-password"
 					/>
-					<FormControlLabel 
-						control={<Checkbox value="remember" color="primary" />}
-						label="Remember me" 
-					/>
-					<Button 
-						type="submit" 
-						fullWidth 
-						variant="contained" 
-						color="primary" 
+					<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+					<Button
+						d
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
 						className={classes.submit}
 						children="Sign In"
 					/>
 					<Grid container>
 						<Grid item xs>
-							<Link href="#" variant="body2">
+							<Link href="#" variant="body2" color="secondary">
 								Forgot password?
 							</Link>
 						</Grid>
 						<Grid item>
-							<Link onClick={handleCreateUserClick} variant="body2">
+							<Link onClick={handleCreateUserClick} variant="body2" color="secondary">
 								{"Don't have an account? Sign Up"}
 							</Link>
 						</Grid>
