d33bcd06e642a12a6fb1e673af806907f612dd25
nxtcoder17 <nxtcoder17@gmail.com>
2020-10-01 09:22:17 +0530

    :bento: Changed Discord Logo Color from Black to White

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -25,7 +25,7 @@ export default function MusicAppBar({
 						children={process.env.REACT_APP_NAME}
 					/>
 					<IconButton href='https://discord.gg/Xkpvnz9' target='_blank'>
-						<img src='https://discord.com/assets/94db9c3c1eba8a38a1fcf4f223294185.png' height='32px' />
+						<img src='https://discord.com/assets/94db9c3c1eba8a38a1fcf4f223294185.png' height='32px' style={{ filter: 'invert(1)' }}/>
 					</IconButton>
 					<EnvironmentBadges />
 				</div>
