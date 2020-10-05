364413fe0e95fc9771442de9bf3255fe17e177c9
Jose <josejardimdev@gmail.com>
2020-10-05 10:31:46 +0200

    updated the meta img from svg to png

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -1,60 +1,251 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, IconButton, InputBase, Badge, MenuItem, Menu } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import VideoSearch from './VideoSearch';
-import UserSection from './UserSection';
-import EnvironmentBadges from './EnvironmentBadges'
-import { makeStyles } from '@material-ui/core/styles';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
-	navBar: {
-		backgroundColor: theme.palette.primary.dark
-	}
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
 }));
 
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-export default function MusicAppBar({
-	handleSubmitVideoSearch,
-	handleSearchTermInput,
-	user,
-	setUser,
-	setGlobalChatOpen
-}) {
-	const classes = useStyles();
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-	return (
-		<AppBar position="static" className={classes.navBar}>
-			<Toolbar
-				display="flex"
-				style={{ justifyContent: 'space-between' }}
-			>
-				<div>
-					<IconButton href='#' >
-						<img src='.\icon-logo.svg' height='48px' />
-					</IconButton>
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-					<Typography
-						display='inline'
-						style={{ marginRight: '20px' }}
-						children={process.env.REACT_APP_NAME}
-					/>
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
 
-					<IconButton href='https://discord.gg/b2gEwT8' target='_blank'>
-						<img src='https://discord.com/assets/e7a3b51fdac2aa5ec71975d257d5c405.png' height='32px' />
-					</IconButton>
-					<EnvironmentBadges />
-				</div>
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
 
-				<VideoSearch
-					handleSearchTermInput={handleSearchTermInput}
-					handleSubmitVideoSearch={handleSubmitVideoSearch}
-				/>
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+  
 
-				<UserSection
-					user={user}
-					setUser={setUser}
-					setGlobalChatOpen={setGlobalChatOpen}
-				/>
-			</Toolbar>
-		</AppBar>
-	);
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Join Us</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={0} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={0} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+
+``		{/* Discord icon mobile */}
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="join the discord support server"
+          aria-haspopup="true"
+		  color="inherit"
+		  href='https://discord.gg/b2gEwT8' 
+		  target='_blank'
+        >
+          <img src='https://discord.com/assets/28174a34e77bb5e5310ced9f95cb480b.png' height='32px' />
+        </IconButton>
+        <p>Discord</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+			{/* Icon-logo */}
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Artistify logo"
+          >
+            <img src='.\icon-logo.svg' height='48px' />            
+          </IconButton>
+
+		  	{/* Artistify name from frontend .env */}
+          <Typography
+            display='inline'
+            style={{ marginRight: '20px' }}
+            children={process.env.REACT_APP_NAME}
+         />
+		 
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+		
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={0} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={0} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+
+			{/* Discord icon desktop */}
+            <IconButton
+              edge="end"
+              aria-label="Discord"
+              aria-controls={menuId}
+              aria-haspopup="true"
+			  color="inherit"
+			  href='https://discord.gg/b2gEwT8' 
+			  target='_blank'
+            >
+              <img src='https://discord.com/assets/28174a34e77bb5e5310ced9f95cb480b.png' height='32px' />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+			  
+            </IconButton>
+          </div>
+        </Toolbar> 
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
 }
