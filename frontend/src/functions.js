17e1ec771d80640db0e3faef2ceb318c00c948e6
ian <ian@unichord-apps.com>
2020-10-05 10:08:05 -0400

    changed frontend to hit prod

--- frontend/src/functions.js
+++ frontend/src/functions.js
@@ -5,10 +5,10 @@ let baseUrl
 if (process.env.NODE_ENV === 'production'){
     baseUrl = process.env.REACT_APP_API_URL_RELATIVE
 } else if (process.env.NODE_ENV === 'development'){
-    baseUrl = process.env.REACT_APP_API_URL_PROD
+    baseUrl = process.env.REACT_APP_API_URL_LOCAL
     
 } else {
-    baseUrl = process.env.REACT_APP_API_URL_PROD
+    baseUrl = process.env.REACT_APP_API_URL_LOCAL
 }
 
 export function setDefaultToken (token) {
