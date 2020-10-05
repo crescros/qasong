23f821026285611b6afb72a04dc2d6bd06a38409
ian <ian@unichord-apps.com>
2020-10-04 21:45:34 -0400

    increased default search result length to 24

--- search/search.service.js
+++ search/search.service.js
@@ -432,7 +432,7 @@ async function searchYoutube({ searchTerm, apiKey }) {
         return searchFixture.items
     } else {
         return axios
-        .get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${searchTerm}&key=${apiKey}`)
+        .get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${searchTerm}&key=${apiKey}`)
         .then((response) => {
             return response.data.items;
         })
