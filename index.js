9f82c7cd90a596fa9338a84445f9e359d0fda136
ian <ian@unichord-apps.com>
2020-10-05 09:44:02 -0400

    disabled db connection and db related endpoints

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const dotenv = require('dotenv').config()
 const rootpath = require('rootpath')()
 
 // database connection 
-const con = require('./database/connection.js')
-con.connect()
+// const con = require('./database/connection.js')
+// con.connect()
 
 // initialize express
 const app = express()
@@ -21,8 +21,8 @@ app.use(jwt())
 app.use(errorHandler)
 
 // define routes
-app.use('/api/globalchat', require('./globalchat/globalchat.controller'))
-app.use('/api/users', require('./users/users.controller'))
+// app.use('/api/globalchat', require('./globalchat/globalchat.controller'))
+// app.use('/api/users', require('./users/users.controller'))
 app.use('/api/search', require('./search/search.controller'))
 app.use('/api/env', (req, res)=> res.send(process.env.NODE_ENV))
 
