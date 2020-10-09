972761c90f226e0e42d39fbb591319e860c578c0
milan090 <milanmuhammed1@gmail.com>
2020-10-09 16:22:47 +0530

    Added winston and morgan to express

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const jwt = require("./_helpers/jwt")
 const errorHandler = require("./_helpers/error-handler")
+const morgan = require("morgan");
 require("dotenv").config()
 require("rootpath")()
+
+const winston = require("./_helpers/winston");
 
 // database connection
 // const con = require('./database/connection.js')
@@ -14,6 +17,9 @@ require("rootpath")()
 // initialize express
 const app = express()
 app.use(cors())
+app.use(morgan("combined", {
+  stream: winston.stream
+}))
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -29,5 +35,6 @@ app.use("/api/env", (req, res) => res.send(process.env.NODE_ENV))
 // start server
 const port = process.env.PORT || 3016
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log("Express server is running at http://localhost:" + port)
 })
