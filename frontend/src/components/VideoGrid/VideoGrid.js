47702a0a908c174d35e5135164fc3e1de59b8e86
callbacc <alexa.matei@icloud.com>
2020-10-17 20:44:04 +0300

    Update VideoGrid.js

--- frontend/src/components/VideoGrid/VideoGrid.js
+++ frontend/src/components/VideoGrid/VideoGrid.js
@@ -22,7 +22,7 @@ function VideoGrid({ videos, nowPlaying, setNowPlaying, queue, setQueue }) {
           const title = video.title;
 
           return (
-            <Grid item xs={12} sm={6} md={4} lg={2} xl={2} key={video.videoId} p={2}>
+            <Grid item xs={12} sm={4} md={3} lg={2} xl={1} key={video.videoId} p={2}>
               <Box m={1}>
                 <VideoCard
                   id={video.videoId}
