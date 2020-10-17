e23efa66d7bd3c680f6588459467ded3358f58fd
callbacc <alexa.matei@icloud.com>
2020-10-17 22:51:43 +0300

    Update PlayArea.js

--- frontend/src/components/footerPlayArea/PlayArea.js
+++ frontend/src/components/footerPlayArea/PlayArea.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core'
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton'; 
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import { borders } from '@material-ui/system';
-
+import { Stop as StopIcon } from "@material-ui/icons";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,18 +23,30 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function BottomAppBar() {
+export default function BottomAppBar({nowPlaying, queue, videos, setNowPlaying}) {
   const classes = useStyles();
 
+function stopTheSong() {
+  setNowPlaying({});
+}
+
+  if(!nowPlaying || !nowPlaying.title){
+    return(
+      <div></div>
+    );
+  };
 
   return (
     <React.Fragment>
       <CssBaseline />
       <AppBar position="fixed" color="primary" className={classes.appBar}>
         <Toolbar className={classes.grow}>
-            <IconButton>
-                <PlayCircleOutlineIcon style={{ fontSize: 35 }} />
-            </IconButton>
+        <Typography>
+          {  nowPlaying.title } 
+        </Typography>
+        <IconButton onClick={stopTheSong}>
+          <StopIcon />
+        </IconButton>
         </Toolbar>
       </AppBar>
     </React.Fragment>
