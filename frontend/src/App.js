42fc892e696768641cbc13dbae306e981492b1f3
callbacc <alexa.matei@icloud.com>
2020-10-17 22:51:38 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,14 +48,7 @@ const App = () => {
   const [user, setUser] = useState();
   const [videos, setVideos] = useState([]);
   const [usersConnected, setUsersConnected] = useState(0);
-  // runs once when app is rendered
-  useEffect(() => {
-    const socket = socketIOClient(process.env.REACT_APP_API_URL_LOCAL);
-    socket.on("usersConnectedUpdate", (usersConnectedResponse) => {
-      setUsersConnected(usersConnectedResponse);
-    });
-    return () => socket.disconnect();
-  }, []);
+
   useEffect(() => {
     (async () => {
       // set dark mode from local storage
@@ -239,7 +232,13 @@ const App = () => {
         }}
       />
 
-      <PlayArea />
+      <PlayArea
+      { ... {
+        nowPlaying,
+        queue,
+        videos,
+        setNowPlaying,
+      }} />
     </ThemeProvider>
   );
 };
