b80f56cdf848d04f207633c4dd8667de03f2c6de
callbacc <alexa.matei@icloud.com>
2020-10-17 20:43:58 +0300

    Update VideoCard.js

--- frontend/src/components/VideoGrid/VideoCard/VideoCard.js
+++ frontend/src/components/VideoGrid/VideoCard/VideoCard.js
@@ -19,13 +19,15 @@ import {
 } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  card: {
     position: "relative",
     maxWidth: 250,
-    height: 190,
     "&:hover > *": {
       visibility: "visible !important",
     }
+  },
+  actionArea:{  
+    height: 190,
   },
   media: {
     height: 125,
@@ -98,8 +100,8 @@ export default function MediaCard({
   }
 
   return (
-      <Card className={classes.root} style={{ backgroundColor: playing && "#2ad156" }}>
-        <CardActionArea onClick={handlePlayButton}>
+      <Card className={classes.card} style={{ backgroundColor: playing && "#2ad156" }}  onClick={handlePlayButton} >
+        <CardActionArea className={classes.actionArea}>
           <CardMedia
             className={classes.media}
             image={thumbnailUrl}
@@ -119,149 +121,3 @@ export default function MediaCard({
   );
 }
 
-
-
-// this is the old cards
-// import React, { useState, useEffect } from "react";
-// import uuid from "react-uuid";
-// import { formatVideoTitle } from "../../../functions";
-// import { makeStyles } from "@material-ui/core/styles";
-// import {
-//   Card,
-//   CardContent,
-//   CardMedia,
-//   IconButton,
-//   Typography,
-//   Tooltip
-// } from "@material-ui/core";
-// import {
-//   PlayArrow as PlayArrowIcon,
-//   Pause as PauseIcon,
-//   Queue as QueueIcon,
-//   Info as InfoIcon
-// } from '@material-ui/icons';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: "flex",
-//     maxWidth: 365,
-//     height: 130,
-//     margin: "0px auto 20px auto",
-//   },
-//   details: {
-//     display: "flex",
-//     flexDirection: "column",
-//   },
-//   content: {
-//     flex: "1 0 auto",
-//   },
-//   cover: {
-//     width: 151,
-//     height: 140,
-//     marginLeft: "auto",
-//   },
-//   controls: {
-//     display: "flex",
-//     alignItems: "center",
-//     paddingLeft: theme.spacing(1),
-//     paddingBottom: theme.spacing(1),
-//   },
-//   playIcon: {
-//     height: 38,
-//     width: 38,
-//   },
-//   titleSize: {
-//     fontSize: 16,
-//   },
-// }));
-
-// export default function MediaControlCard({
-//   title,
-//   description,
-//   thumbnailUrl,
-//   smallThumbnailUrl,
-//   id,
-//   setNowPlaying,
-//   nowPlaying,
-//   queue,
-//   setQueue,
-// }) {
-//   const classes = useStyles();
-//   const [playing, setPlaying] = useState(false);
-
-//   useEffect(() => {
-//     if (nowPlaying && nowPlaying.id === id) {
-//       setPlaying(true);
-//     } else {
-//       setPlaying(false);
-//     }
-//   }, [nowPlaying]);
-
-//   function handlePlayButton() {
-//     if (!playing) {
-//       setNowPlaying({
-//         title: title,
-//         description: description,
-//         id: id,
-//         thumbnailUrl: thumbnailUrl,
-//       });
-//       setPlaying(true);
-//     } else {
-//       setNowPlaying({});
-//       setPlaying(false);
-//     }
-//   }
-
-  // function handleAddQueue() {
-  //   setQueue(
-  //     queue.concat({
-  //       title: title,
-  //       description: description,
-  //       id: id,
-  //       qid: uuid(),
-  //       thumbnailUrl: thumbnailUrl,
-  //       smallThumbnailUrl: smallThumbnailUrl,
-  //     })
-  //   );
-  // }
-
-//   return (
-//     <Card className={classes.root} style={{ backgroundColor: playing && "#2ad156" }}>
-//       <div className={classes.details}>
-//         <CardContent className={classes.content} style={{ height: "60px" }}>
-//           <Typography className={classes.titleSize} component="h5" variant="h5">
-//             {formatVideoTitle(title)}
-//           </Typography>
-//         </CardContent>
-//         <div className={classes.controls}>
-
-//           {/* play/stop button */}
-//           <Tooltip title={playing ? "stop" : "play"}>
-//             <IconButton size="small" onClick={handlePlayButton} >
-//               {playing ? <PauseIcon /> : <PlayArrowIcon />}
-//             </IconButton>
-//           </Tooltip>
-
-//           {/* queue button */}
-//           <Tooltip title="add to queue">
-//             <IconButton size="small" onClick={handleAddQueue}>
-//               <QueueIcon />
-//             </IconButton>
-//           </Tooltip>
-
-//           <Tooltip title={description} >
-//             <InfoIcon color="disabled" />
-//           </Tooltip>
-
-//         </div>
-//       </div>
-//       <CardMedia
-//         className={classes.cover}
-//         component="img"
-//         alt={title}
-//         image={thumbnailUrl}
-//         title={title}
-//       />
-//     </Card>
-//   );
-// }
