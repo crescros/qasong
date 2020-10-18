71898b8a8d046507964a3f88bf8fefc2d80b05d9
callbacc <alexa.matei@icloud.com>
2020-10-18 13:54:14 +0300

    Update PlayArea.js

--- frontend/src/components/footerPlayArea/PlayArea.js
+++ frontend/src/components/footerPlayArea/PlayArea.js
@@ -6,29 +6,35 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton'; 
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-import { borders } from '@material-ui/system';
-import { Stop as StopIcon } from "@material-ui/icons";
+import border from '@material-ui/system';
 import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
+
+
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     top: 'auto',
     bottom: 0,
     height: 75,
+    borderColor: 'red',
+    backgroundColor: "#0f0f0f"
   },
   grow: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  border: {
+    borderColor: '#fff',
+  }
 }));
 
 export default function BottomAppBar({nowPlaying, queue, videos, setNowPlaying}) {
   const classes = useStyles();
 
-function stopTheSong() {
-  setNowPlaying({});
-}
+  function stopTheSong() {
+    setNowPlaying({});
+  }
 
   if(!nowPlaying || !nowPlaying.title){
     return(
