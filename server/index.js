4f654c2154fc27421ab669b7d5bf222604035a1f
milan090 <milanmuhammed1@gmail.com>
2020-10-18 08:39:47 +0530

    fixed issues

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 // import dependencies
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
 const bodyParser = require("body-parser");
 const jwt = require("./_helpers/jwt");
 const errorHandler = require("./_helpers/error-handler");
+const socketIo = require("socket.io");
 require("dotenv").config();
 require("rootpath")();
 
@@ -25,13 +27,29 @@ app.use(bodyParser.json());
 app.use(jwt());
 app.use(errorHandler);
 
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let usersConnected = 0;
+
+io.on("connection", (socket) => {
+  usersConnected++;
+  const interval = setInterval(() => {
+    socket.emit("usersConnectedUpdate", usersConnected);
+  }, 10000);
+  socket.on("disconnect", () => {
+    usersConnected--;
+    clearInterval(interval);
+  });
+});
+
 // define routes
 app.use("/api/search", require("./search/search.controller"));
 app.use("/api/env", (req, res) => res.send(process.env.NODE_ENV));
 
 // start server
 const port = process.env.PORT || 3016;
-app.listen(port, () => {
+server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log("Express server is running at http://localhost:" + port);
 });
