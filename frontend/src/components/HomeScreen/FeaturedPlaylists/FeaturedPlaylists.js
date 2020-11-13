1f3d6c10e339b3a022ffc54a8253f0f774071327
ian <ian@unichord-apps.com>
2020-11-12 22:15:56 -0500

    format

--- frontend/src/components/HomeScreen/FeaturedPlaylists/FeaturedPlaylists.js
+++ frontend/src/components/HomeScreen/FeaturedPlaylists/FeaturedPlaylists.js
@@ -62,8 +62,8 @@ function FeaturedPlaylists({
           />
         );
       })}
-   
-      <JoinUsOnDiscord />   
+
+      <JoinUsOnDiscord />
     </div>
   );
 }
--- frontend/src/components/HomeScreen/FeaturedPlaylists/JoinUsOnDiscord/JoinUsOnDiscord.js
+++ frontend/src/components/HomeScreen/FeaturedPlaylists/JoinUsOnDiscord/JoinUsOnDiscord.js
@@ -1,60 +1,51 @@
 import React from "react";
-import {
-    CardActionArea,
-    Grid,
-    Typography,
-} from "@material-ui/core";
+import { CardActionArea, Grid, Typography } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    playlist: {
-        borderColor: "#ffae5c",
-        border: "2px solid",
-        maxWidth: 500,
-        margin: "auto",
-        padding: theme.spacing(4),
-        marginBottom: theme.spacing(4),
-    },
-    svg: {
-        filter: theme.palette.type === "dark" ? "none" : "invert(1)",
-        width: "64px"
-    }
+  playlist: {
+    borderColor: "#ffae5c",
+    border: "2px solid",
+    maxWidth: 500,
+    margin: "auto",
+    padding: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+  },
+  svg: {
+    filter: theme.palette.type === "dark" ? "none" : "invert(1)",
+    width: "64px",
+  },
 }));
 
-function Playlist({
+function Playlist() {
+  const classes = useStyles();
 
-}) {
-    const classes = useStyles();
-
-
-    return (
-        <Grid
-            key={"discord"}
-            item
-        >
-            <CardActionArea
-                href="https://discord.com/invite/kczTtg8VDP"
-                className={classes.playlist}>
-                <Grid container alignContent="center" justify="center" alignItems="center">
-                    <Grid item xs={12} sm={3}>
-                        <img className={classes.svg} src="./img/discord.svg"></img>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Typography align="center" color="secondary">
-                            Join Us On Discord
-                        </Typography>
-                        <Typography align="center" variant="body2" color="textSecondary">
-                            contact the Qasong Team
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={3}>
-                        <img width="80px" src="./img/qasong.svg"></img>
-                    </Grid>
-                </Grid>
-            </CardActionArea>
+  return (
+    <Grid key={"discord"} item>
+      <CardActionArea
+        href="https://discord.com/invite/kczTtg8VDP"
+        className={classes.playlist}
+      >
+        <Grid container alignContent="center" justify="center" alignItems="center">
+          <Grid item xs={12} sm={3}>
+            <img className={classes.svg} src="./img/discord.svg"></img>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography align="center" color="secondary">
+              Join Us On Discord
+            </Typography>
+            <Typography align="center" variant="body2" color="textSecondary">
+              contact the Qasong Team
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <img width="80px" src="./img/qasong.svg"></img>
+          </Grid>
         </Grid>
-    );
+      </CardActionArea>
+    </Grid>
+  );
 }
 
 export default Playlist;
