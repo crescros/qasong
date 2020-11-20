51a769b93c0a5d3e94cee735d9622656b2033f07
ian <ian@unichord-apps.com>
2020-11-19 20:21:41 -0500

    fixed tests

--- frontend/src/components/QueueSection/QueueSection.test.js
+++ frontend/src/components/QueueSection/QueueSection.test.js
@@ -5,12 +5,12 @@ import QueueSection from "./QueueSection";
 
 describe("<QueueSection />", () => {
   it("renders an empty div if id is falsy", () => {
-    const wrapper = shallow(<QueueSection />);
+    const wrapper = shallow(<QueueSection  queue={[{}]} />);
     expect(wrapper.find("div")).toHaveLength(1);
   });
 
   it("renders the Youtube component if id is truthy", () => {
-    const wrapper = shallow(<QueueSection showQueue={true} queue={[{}]} />);
+    const wrapper = shallow(<QueueSection  queue={[{}]} />);
     expect(wrapper.find(DndProvider)).toHaveLength(1);
   });
 });
