1815e3d0f9d23dca6fdd318c315ac537368e739a
unjown <71381907+unjown@users.noreply.github.com>
2020-11-25 09:34:27 +0800

    Update NotFound.js
    update /

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 import { useLocation } from "react-router-dom";
-import logo from '.404.png';
+import logo from './404.png';
 
 function NotFound() {
   const location = useLocation();
