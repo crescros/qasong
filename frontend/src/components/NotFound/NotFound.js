c16eb35a7d85768bf46917e517e217438ac3f6e5
unjown <71381907+unjown@users.noreply.github.com>
2020-11-25 09:28:59 +0800

    Create NotFound.js
    404 page update

---
+++ frontend/src/components/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import { useLocation } from "react-router-dom";
+import logo from '.404.png';
+
+function NotFound() {
+  const location = useLocation();
+  return (
+    <>
+<img src={logo} alt="404" />
+    </>
+  );
+}
+
+export default NotFound;
