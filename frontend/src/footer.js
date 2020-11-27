573cc5988bead097ea41d002de2332799ac51046
unjown <71381907+unjown@users.noreply.github.com>
2020-11-27 10:17:39 +0800

    Update footer.js

--- frontend/src/footer.js
+++ frontend/src/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <div className="footer">
     <a href={qasong.com/about_us}>About us </a> 
-    <a href={qasong.com/support} Support </a>
+    <a href={qasong.com/support}> Support </a>
   </div>
 );
 
