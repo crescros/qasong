38639c9c51e8687e5b9279c31bd02912acff4b36
Ian Walston <ikwalston@comcast.net>
2020-12-01 18:46:09 -0500

    lint

--- frontend/src/components/HomeScreen/FeaturedPlaylists/FeedItem/Playlist/Playlist.js
+++ frontend/src/components/HomeScreen/FeaturedPlaylists/FeedItem/Playlist/Playlist.js
@@ -118,9 +118,11 @@ function Playlist({
           </Link>
         </Typography>
 
-        {
-          playlist.author && <Typography variant="h6" align="center" color="textSecondary">{playlist.author}</Typography>
-        }
+        {playlist.author && (
+          <Typography variant="h6" align="center" color="textSecondary">
+            {playlist.author}
+          </Typography>
+        )}
 
         <Typography color="textSecondary" align="center">
           {playlist.queue.length} songs, {duration}
