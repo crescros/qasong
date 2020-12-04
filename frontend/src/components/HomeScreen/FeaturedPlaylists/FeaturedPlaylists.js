12dc1363ec3b7a3892da4b9254d041e4c5736729
Ian Walston <ikwalston@comcast.net>
2020-12-04 01:19:46 -0500

    load on scroll

--- frontend/src/components/HomeScreen/FeaturedPlaylists/FeaturedPlaylists.js
+++ frontend/src/components/HomeScreen/FeaturedPlaylists/FeaturedPlaylists.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Grid,
-  Typography,
-  Box,
-  Button,
-} from "@material-ui/core";
+import { Grid, Typography, Box, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { getFeed } from "../../../functions";
 import FeedItem from "./FeedItem/FeedItem";
