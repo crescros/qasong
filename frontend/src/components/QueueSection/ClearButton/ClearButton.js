964ae7eac31bb59c6501df21566f6550ac3eb030
1746924261 <54863160+1746924261@users.noreply.github.com>
2020-12-11 17:50:01 -0500

    Updated Clear Queue Confirm Dialog to Use Material-UI

--- frontend/src/components/QueueSection/ClearButton/ClearButton.js
+++ frontend/src/components/QueueSection/ClearButton/ClearButton.js
@@ -1,23 +1,15 @@
 import React from "react";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import { IconButton, Box } from "@material-ui/core";
-import { clear } from "../../../functions";
+// import { clear } from "../../../functions";
 
-function ShareButton({ disabled, queue, setQueue }) {
-  function handleClick() {
-    let confirmClear = confirm("Are you sure you want to clear the queue?");
-    if (confirmClear) {
-      setQueue(clear(queue));
-    }
-  }
-
+function ShareButton({ disabled }) {
   return (
     <Box mt={1}>
       <IconButton
         edge="end"
         title="Clear the entire queue"
         disabled={disabled}
-        onClick={handleClick}
         target="_blank"
         color={disabled ? "inherit" : "secondary"}
       >
---
+++ frontend/src/components/QueueSection/ConfirmClearDialog/ConfirmClearDialog.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { clear } from "../../../functions";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+  Button,
+} from "@material-ui/core";
+
+function ConfirmClearDialog(props) {
+  const { queue, setQueue, confirmDialog, setConfirmDialog } = props;
+
+  function handleClickYes() {
+    setQueue(clear(queue));
+  }
+
+  function handleClickNo() {
+    setConfirmDialog({ ...confirmDialog, isOpen: false });
+  }
+
+  return (
+    <Dialog
+      open={confirmDialog.isOpen}
+      PaperProps={{ style: { backgroundColor: "orange", boxShadow: "none" } }}
+    >
+      <DialogTitle>
+        <Typography variant="h6" color="primary">
+          {confirmDialog.title}
+        </Typography>
+      </DialogTitle>
+
+      <DialogContent>
+        <Typography variant="subtitle2" color="primary">
+          {confirmDialog.subTitle}
+        </Typography>
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={handleClickYes}>
+          <Typography color="primary"> Yes </Typography>
+        </Button>
+        <Button onClick={handleClickNo}>
+          <Typography color="primary"> No </Typography>
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default ConfirmClearDialog;
