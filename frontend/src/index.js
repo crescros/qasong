1daf0c7c71e5a6f9286064650307f00a96e46f96
Ian Walston <ikwalston@comcast.net>
2020-12-10 19:22:17 -0500

    http to https redirect in gae

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,3 @@
-// if (process.env.NODE_ENV !== "development") {
-//   // redirect to https
-//   if (location.protocol === "http:") {
-//     location.href = "https://qasong.com" + location.pathname;
-//   }
-// }
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
